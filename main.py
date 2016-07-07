fe44eb5acd1ccd8483028efe00bbbe81cecd63f2
AlexanderMamaev <alxmamaev@mail.ru>
2016-07-07 21:26:43 +0300

    add new command and code editor

--- main.py
+++ main.py
@@ -1,49 +1,107 @@
 # -*- coding: utf-8 -*-
+import codecs
+
 stack = []
+labels = dict()
+code = []
+line_number = 0
 
 def Error():
-    print("Error!")
+    print("Гррр!")
 
-def dog_sum(values):
+def dog_sum(value):
     if len(stack) > 1: stack.append(stack.pop(-1)+stack.pop(-1))
     else: Error()
 
-def dog_sub(values):
+def dog_sub(value):
     if len(stack) > 1: stack.append(stack.pop(-1)-stack.pop(-1))
     else: Error()    
 
-def dog_mult(values):
+def dog_mult(value):
     if len(stack) > 1: stack.append(stack.pop(-1)*stack.pop(-1))
     else: Error()   
 
-def dog_div(values):
+def dog_div(value):
     if len(stack) > 1: stack.append(stack.pop(-1)//stack.pop(-1))
     else: Error()     
 
-def dog_input(values):
-    value = input("<< ")
-    if value.isdigit(): stack.append(int(value))
+def dog_input(value):
+    input_value = input("<< ")
+    if input_value.isdigit(): stack.append(int(input_value))
     else: Error()
     
-def dog_output(values):
+def dog_output(value):
     if len(stack) > 0: print(">>",stack.pop(-1))
 
+def dog_push(value):
+    if value.isdigit(): stack.append(int(value[1]))
+    else: Error()
+
+def dog_get(value):
+    if stack: stack.pop(-1)
+
+def dog_stack(value):
+    print("HEAD <<",stack[::-1])
+
+def dog_flip(value):
+    global stack
+    if len(stack) > 1: stack += [stack.pop(-1),stack.pop(-1)]
+    else: Error()
+
+def dog_label(value):
+    if value and labels.get(value) is None: labels[value] = line_number
+    else: Error()
+
+def dog_goto(value):
+    global line_number
+    if value and labels.get(value) is not None:
+        if stack[-1] == 0: line_number = labels[value]
+    else: Error()
+        
 cmd = {
 "тяф!":dog_input,
 "гав!":dog_output,
 "вов!": dog_sum,
 "ваф!": dog_sub,
 "ряф!": dog_mult,
-"вуф": dog_div
+"вуф!": dog_div,
+"ряв!":dog_push,
+"рав!":dog_get,
+"руф!":dog_flip,
+"рав?":dog_stack,
+"хыр": dog_label,
+"рых":dog_goto
+
 }
 
 def parser(ln):
-    ln = ln.split()
+    ln = ln.strip().split()
     if ln:
-        (command, values) = ln[0], ln[1] if len(ln) > 1 else ""
+        (command, value) = ln[0], ln[1] if len(ln) > 1 else ""
         if cmd.get(command) is not None:
-            cmd.get(command)(values)
+            cmd.get(command)(value)
+        else: Error()
     else: Error()
 
-while True:
-    parser(input())
+def run_code():
+    global line_number
+    print("=====================")
+    while line_number < len(code): 
+        parser(code[line_number])
+        line_number += 1
+
+def editor():
+    print("=====================")
+    global code
+    line = input()
+    while line != "рюх.":
+        code.append(line)
+        line = input()
+    run_code()
+
+def main():
+    while True:
+        comand = input()
+        if comand == "груг!": editor()
+
+if __name__ == "__main__": main()
